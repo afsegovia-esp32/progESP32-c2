b06d4f7971ccaee00fa286cbd259ff576887d65c
afsegovia-esp32 <afsegovia.dev@gmail.com>
2026-07-09 01:31:14 +0000

    S3-p2

--- m1-fundamentos/03_variables_constantes.c
+++ m1-fundamentos/03_variables_constantes.c
@@ -13,9 +13,13 @@
 #include <stdbool.h>
 
 /* TODO: definir con #define: PIN_LED=2, VOLTAJE_NOMINAL=3.3f, MAX_SENSORES=8 */
-
+#define PIN_LED 2
+#define VOLTAJE_NOMINAL 3.3f
+#define MAX_SENSORES 8
 /* TODO: declarar con const: const uint8_t FRECUENCIA_HZ = 10 */
 /*                           const float   UMBRAL_TEMP_C = 35.0f */
+const uint8_t FRECUENCIA_HZ = 10;
+const float   UMBRAL_TEMP_C = 35.0f;
 
 int main(void) {
 
@@ -23,13 +27,26 @@ int main(void) {
     /* TODO: declarar e inicializar:
      *   int32_t contador=0, float temperatura=25.5f,
      *   uint8_t nivel_bateria=87, bool led_activo=false, char estado='I'
-     */
+    */
+    int32_t contador=0;
+    float temperatura=25.5f;
+    uint8_t nivel_bateria=87;
+    bool led_activo=false;
+    char estado='I';
 
     /* TODO: imprimir estado inicial */
+    printf("Valores iniciales: contador = %d temp=%.1f bat=%d led=%d estado=%c \n", contador, temperatura, nivel_bateria, led_activo, estado);
 
     /* TODO: modificar todas las variables a nuevos valores */
+    contador=42;
+    temperatura=37.2f;
+    nivel_bateria=55;
+    led_activo=true;
+    estado='A';
 
     /* TODO: imprimir estado modificado */
+    printf("Valores iniciales: contador = %d temp=%.1f bat=%d led=%d estado=%c \n", contador, temperatura, nivel_bateria, led_activo, estado);
+
 
     printf("\n=== Constantes #define ===\n");
     /* TODO: imprimir PIN_LED, VOLTAJE_NOMINAL, MAX_SENSORES */
@@ -39,6 +56,7 @@ int main(void) {
 
     /* Descomenta para ver el error de compilacion:
      * FRECUENCIA_HZ = 20;   -> error: assignment of read-only variable */
+    printf("El valor de frecuencia es : %u \n", FRECUENCIA_HZ);
 
     return 0;
 }
